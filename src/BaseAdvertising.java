908e4c8e76fcf0883f6657d45c89c2ee0fcb00bc
Ali Shahamtnia <ali.shahamatnia@gmail.com>
2021-07-11 05:44:42 +0430

    unique id handled

--- src/BaseAdvertising.java
+++ src/BaseAdvertising.java
@@ -1,7 +1,15 @@
 public class BaseAdvertising {
-    private int id;
+    private static int objects_count;
+    private final int id;
     private int views;
     private int clicks;
+
+    public BaseAdvertising() {
+        this.views = 0;
+        this.clicks = 0;
+        this.id = objects_count;
+        objects_count++;
+    }
 
     public int getId() {
         return id;
@@ -22,7 +30,8 @@ public class BaseAdvertising {
     public void incClicks() {
         clicks++;
     }
-    public String describeMe(){
+
+    public String describeMe() {
         return "this is an object of BaseAdvertising class which has common " +
                 "fields and methods of Ad and Advertiser classes, this class could be abstract";
     }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
         BaseAdvertising baseAdvertising = new BaseAdvertising();
+
         UI ui = new UI();
 
         ui.print_message(baseAdvertising.describeMe());
